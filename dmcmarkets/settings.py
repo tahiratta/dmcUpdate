0ab69d1a2a3a902ffdcd3911457c4c2286a42fda
Tahir <tahiratta27@gmail.com>
2018-03-21 10:03:51 -0700

    very last

--- dmcmarkets/settings.py
+++ dmcmarkets/settings.py
@@ -77,25 +77,25 @@ WSGI_APPLICATION = 'dmcmarkets.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dmc',
-        'USER': 'root',
-        'PASSWORD': 'tahir123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
-        #'default-character-set' = 'utf8',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'dmc',
+#         'USER': 'root',
+#         'PASSWORD': 'tahir123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+
+#         #'default-character-set' = 'utf8',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
